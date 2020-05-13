302ec399962148003ab9510c58ee35df0f48f476
Zheyuan Zhang <56742253+JasonZhangzy1757@users.noreply.github.com>
2020-05-13 16:58:32 -0400

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -60,16 +60,81 @@ def make_weights_for_balanced_classes(classes, n_classes):
 
 
 class BaseTrainer(object):
-    def __init__(self, args):
+    def __init__(self, args, lamda=40):
         self.args = args
         self.set_random_seed(random_seed=args.random_seed)
+        self.lamda = lamda
 
         self.tokenizer = BertTokenizer.from_pretrained(args.bert_model,
                                                        do_lower_case=args.do_lower_case)
         if args.debug:
             print("Debugging mode on.")
         self.features_lst = self.get_features(self.args.train_folder, self.args.debug)
+        
+     
+    def estimate_fisher(self, dataset, sample_size, batch_size=32):
+        # sample loglikelihoods from the dataset.
+        data_loader = utils.get_data_loader(dataset, batch_size)
+        loglikelihoods = []
+        for x, y in data_loader:
+            x = x.view(batch_size, -1)
+            x = Variable(x).cuda() if self._is_on_cuda() else Variable(x)
+            y = Variable(y).cuda() if self._is_on_cuda() else Variable(y)
+            loglikelihoods.append(
+                F.log_softmax(self(x), dim=1)[range(batch_size), y.data]
+            )
+            if len(loglikelihoods) >= sample_size // batch_size:
+                break
+        # estimate the fisher information of the parameters.
+        loglikelihoods = torch.cat(loglikelihoods).unbind()
+        loglikelihood_grads = zip(*[autograd.grad(
+            l, self.parameters(),
+            retain_graph=(i < len(loglikelihoods))
+        ) for i, l in enumerate(loglikelihoods, 1)])
+        loglikelihood_grads = [torch.stack(gs) for gs in loglikelihood_grads]
+        fisher_diagonals = [(g ** 2).mean(0) for g in loglikelihood_grads]
+        param_names = [
+            n.replace('.', '__') for n, p in self.named_parameters()
+        ]
+        return {n: f.detach() for n, f in zip(param_names, fisher_diagonals)}
 
+    
+    def consolidate(self, fisher):
+        for n, p in self.named_parameters():
+            n = n.replace('.', '__')
+            self.register_buffer('{}_mean'.format(n), p.data.clone())
+            self.register_buffer('{}_fisher'
+                                 .format(n), fisher[n].data.clone())
+
+    def ewc_loss(self, cuda=False):
+        try:
+            losses = []
+            for n, p in self.named_parameters():
+                # retrieve the consolidated mean and fisher information.
+                n = n.replace('.', '__')
+                mean = getattr(self, '{}_mean'.format(n))
+                fisher = getattr(self, '{}_fisher'.format(n))
+                # wrap mean and fisher in variables.
+                mean = Variable(mean)
+                fisher = Variable(fisher)
+                # calculate a ewc loss. (assumes the parameter's prior as
+                # gaussian distribution with the estimated mean and the
+                # estimated cramer-rao lower bound variance, which is
+                # equivalent to the inverse of fisher information)
+                losses.append((fisher * (p-mean)**2).sum())
+            return (self.lamda/2)*sum(losses)
+        except AttributeError:
+            # ewc loss is 0 if there's no consolidated parameters.
+            return (
+                Variable(torch.zeros(1)).cuda() if cuda else
+                Variable(torch.zeros(1))
+            )
+       
+    
+    def _is_on_cuda(self):
+        return next(self.parameters()).is_cuda
+    
+    
     def make_model_env(self, gpu, ngpus_per_node):
         if self.args.distributed:
             self.args.gpu = self.args.devices[gpu]
@@ -84,7 +149,7 @@ class BaseTrainer(object):
                                     world_size=self.args.world_size, rank=self.args.rank)
 
         # Load baseline model
-        self.model = EWCoverQA.from_pretrained(self.args.bert_model)
+        self.model = BertForQuestionAnswering.from_pretrained(self.args.bert_model)
 
         if self.args.load_model is not None:
             print("Loading model from ", self.args.load_model)
@@ -263,7 +328,7 @@ class BaseTrainer(object):
                     loss = loss.mean()
                     loss = loss / self.args.gradient_accumulation_steps
                     
-                    ewc_loss = self.model.ewc_loss(cuda=cuda)
+                    ewc_loss = self.ewc_loss(cuda=cuda)
                     loss = loss + ewc_loss
                     
                     loss.backward()
@@ -297,18 +362,18 @@ class BaseTrainer(object):
                 for dev_file, f1 in result_dict.items():
                     print("GPU/CPU {} evaluated {}: {:.2f}".format(self.args.gpu, dev_file, f1), end="\n")
         
-        if consolidate:
-            # estimate the fisher information of the parameters and consolidate
-            # them in the network.
-            print(
-                '=> Estimating diagonals of the fisher information matrix...',
-                flush=True, end='',
-            )
-            # ATTENTION!!! the data_loader should entire training set!!!!
-            self.model.consolidate(self.model.estimate_fisher(
-                data_loader, fisher_estimation_sample_size
-            ))
-            print('EWC Loaded!')
+        #if consolidate:
+        #    # estimate the fisher information of the parameters and consolidate
+        #    # them in the network.
+        #    print(
+        #        '=> Estimating diagonals of the fisher information matrix...',
+        #        flush=True, end='',
+        #    )
+        #    # ATTENTION!!! the data_loader should entire training set!!!!
+        #    self.model.consolidate(self.model.estimate_fisher(
+        #        data_loader, fisher_estimation_sample_size
+        #    ))
+        #    print('EWC Loaded!')
 
     def evaluate_model(self, epoch):
         # result directory
