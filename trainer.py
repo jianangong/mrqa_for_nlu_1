d4846544362dae79824e68b098cd4048bdcf8418
Zheyuan Zhang <56742253+JasonZhangzy1757@users.noreply.github.com>
2020-05-14 16:58:54 -0400

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -117,7 +117,7 @@ class BaseTrainer(object):
             #
             #sequence_output = torch.stack(outputs[0])
             #logits = self.qa_outputs(sequence_output)
-            log_prob = F.log_softmax(torch.stack(loss), dim=1)
+            log_prob = F.log_softmax(loss, dim=1)
            
             loglikelihoods.append(log_prob)
                 
