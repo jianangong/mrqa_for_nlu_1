067e715ea3cb9426621f89947ad992e57805b8fe
jianangong <60515427+jianangong@users.noreply.github.com>
2020-05-15 00:16:19 -0400

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -104,12 +104,13 @@ class BaseTrainer(object):
             end_positions = end_positions.cuda(self.args.gpu, non_blocking=True)
 
             model = self.bert.to('cuda')
-
-            logits = self.qa_outputs(torch.stack(model(
+            x=model(
                input_ids,
                attention_mask=input_mask,
                token_type_ids=seg_ids
-            )[0]))
+            )[0])
+            x=torch.stack(x)
+            logits = self.qa_outputs(x)
             log_prob = F.log_softmax(logits, dim=0)
             #log_prob = F.log_softmax(torch.rand(len(seq_len),1), dim=0)
             loglikelihoods.append(log_prob)
